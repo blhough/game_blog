683346ac404263add596b19b2dd1ad00b42adeec
Erick <esericksanc@gmail.com>
2015-05-24 09:43:56 -0700

    exe 'scaffold Faq'
    rails generate scaffold Faq question:string answer:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :faqs
   resources :blog_comments
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
